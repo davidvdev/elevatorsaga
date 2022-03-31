d766628035dda5d4824adf773f143d29f34a29c2
David Vogel <david@davidvdev.com>
2022-03-31 10:58:10 -0500

    hopefully a solid foundation for future challenges

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
+        
+        //shared elevator logic
+        elevators.forEach(elevator => {
+            elevator.on('floor_button_pressed', (floorNum) => elevator.goToFloor(floorNum))
+            
+            elevator.on('passing_floor', (floorNum, direction) => {
+                console.log('passing: ', floorNum, ' going: ', direction)
+                if(floors[floorNum].serviceRequest && floors[floorNum].serviceDirection === direction ) elevator.goToFloor(floorNum, true) 
+            })
+            
+            elevator.on('stopped_at_floor', (floorNum) => floors[floorNum].serviceRequest = false)
 
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        elevator.on("idle", function() {
-            // let's go to all the floors (or did we forget one?)
-            elevator.goToFloor(0);
-            elevator.goToFloor(1);
-        });
+            elevator.on("idle", () => {
+                // look through the current requests and head to the nearest floor
+                const requests = floors.filter(floor => floor.serviceRequest)
+                
+                if(requests.length > 0){
+                    elevator.goToFloor(requests[0].floorNum())
+                } else {
+                    elevator.goToFloor(0)
+                }
+                
+            });
+        })
+
+       //shared floor logic
+        floors.forEach(floor => {
+            floor.on('up_button_pressed', () => {
+                floor.serviceRequest = true
+                floor.serviceDirection = 'up'
+            })
+            floor.on('down-button_pressed', () => {
+                floor.serviceRequest = true
+                floor.serviceDirection = 'down'
+            })
+        })
     },
     update: function(dt, elevators, floors) {
         // We normally don't need to do anything here
